67cb7485f43472c0afc5e077b5d2c178c69118d9
Tanguy Colleville <tanguy.colleville@student-cs.fr>
2020-12-30 11:47:43 +0100

    complément rapport+commentaire+ dev du readme

--- README.md
+++ README.md
@@ -13,6 +13,11 @@ Un dossier rapport latex qui contient notre fichier pdf, qui est notre rapport a
 Un fichier requirement.txt qui regroupe l'ensemble des packages nécessaire au lancement et bon fonctionnement de notre modèle. 
 Un fichier readme.md qui vient en aide à l'utilisateur de ce projet. 
 
+# Rapport 
+
+Le rapport pdf se trouve dans le dossier rapport_latex, il vous suffit d'ouvrir l'unique fichier pdf qui se trouve dans ce dossier. 
+
+
 # Pour lancer le programme
 
 Il vous suffit de 
@@ -34,9 +39,17 @@ Vous êtes à présent de en mesure d'utiliser notre modèle.
 # Le programme 
 
 Ouvrir le programme et le lancer en veillant à sélectionner votre environnement virtuel comme interpreteur. 
-Une fenêtre va s'ouvrir.
+Les graphiques vont s'ouvrir un à un. Il suffit de fermer un graphique pour que le suivant s'affiche.
+Vous pouvez à présent jouir de la liberté d'ajuster les différents paramètres du modèle, en sachant que chaque paramètre bénéficie d'une explication détaillée dans l'annexe de ce projet.
+Attention, pour simuler l'abscence de sardine au début il ne suffit pas de mettre la population initiale à 0 d'autres paramètres sont à régler conformément aux explications données en annexe.
 
 # Conclusion 
 On peut dire que nos résultats en termes d'analyse temporelle corresponde à nos attendes. La littérature nous également permis de déterminer les valeurs des paramètres dans le système de Lotka Volterra afin de modéliser l'évolution des populations de sardines et de requins dans la mer Adriatique. Plus de détail dans notre rapport PDF. 
-# Références 
+
+# Références
+https://www.scipy.org/
+https://numpy.org/
+http://ipython.org/
+https://matplotlib.org/
+http://www.banquept.fr/documents/2014/MementoPythonScilab.pdf
 
--- dev/main.py
+++ dev/main.py
@@ -3,49 +3,79 @@
 import matplotlib.pyplot as plt 
 import numpy as np
 import scipy.integrate as spi
-from mpl_toolkits import mplot3d
+# from mpl_toolkits import mplot3d
 
-# u0=1# valeur initiale 
-# v0=1
-u0=1.1# valeur initiale 
+# valeurs initiales
+u0=1.1
 v0=0.1
 w0=0.16
 
 #valeurs des paramètres du modèle
-b1 = 0.01
-k1 = 0.9
-k2 = 0.05
-a2 = 0.7
-c1 = 0.06
-c2 = 0.8
-a1 = 0.5
-q = 0.09
+b1 = 0.01 # représente 
+k1 = 0.9 # représente 
+k2 = 0.05 # représente 
+a2 = 0.7 # représente 
+c1 = 0.06 # représente 
+c2 = 0.8 # représente 
+a1 = 0.5 # représente 
+q = 0.09 # représente 
 lmda = 0.5 # lambda
-p = 0.5
-c = 0.7
-delta  = 0.03
-m=0.9262
+p = 0.5 # représente 
+c = 0.7 # représente 
+delta  = 0.03 # représente 
+m=0.9262 # représente 
+
+## Calcul des points d'équilibres
+
 u_eq=c/(m*q*p)
 v_eq=(a2/c2)*(k2+c/(m*q*p))
 w_eq=(1/(m*q)*((a1*p*m*q-b1*c)/(p*m*q)-(c1*a2*(c+k2*p*m*q))/(c2*(c+k1*p*m*q))))
 
-def F(Vector,t):# problème de Cauchy 
+## fonction qui pose le problème de Cauchy à des fins de résolutions du système différentiel avec scipy
+def F(Vector,t):# ici on parle de système autonome car f ne dépend que de y
     result=[]
     result.append((a1-b1*Vector[0]-(c1*Vector[1])/(Vector[0]+k1))*Vector[0]-m*q*Vector[0]*Vector[2])
     result.append((a2-c2*Vector[1]/(Vector[0]+k2))*Vector[1])
     result.append(lmda*(p*m*q*Vector[0]-c)*Vector[2])
     return result
     
-Y0=[u0,v0,w0]# Vecteur initiale
+Y0=[u0,v0,w0]# Vecteur initial du problème de Cauchy
+
 N=1000# Nombre de valeurs 
 t_final = 100 # temps final
-LT=np.linspace(0,t_final,N)
+
+LT=np.linspace(0,t_final,N) # création de l'intervalle de résolution
+
 S=spi.odeint(F,Y0,LT)# résolution du problème de Cauchy 
+
+# récupération des solutions pour chaque variable
 solU=[S[i][0] for i in range(N)]
 solV=[S[i][1] for i in range(N)]
 solW=[S[i][2] for i in range(N)]
+
+# création d'une liste des solutions 
 Sol=[solU,solV,solW]
 
+
+# mise en oeuvre d'une résolution via Euler explicite du première ordre
+
+u_euler = [u0]
+v_euler = [v0]
+w_euler = [w0]
+u_prim_euler = [0]
+v_prim_euler = [0]
+w_prim_euler = [0]
+h = t_final/N
+for i in range(len(LT)-1):
+    u_euler.append(u_euler[i]  + h * ( (a1 - b1 * u_euler[i] - ((c1*v_euler[i]) / (u_euler[i]+k1))) * u_euler[i] - m*q * u_euler[i] * w_euler[i]) )
+    v_euler.append(v_euler[i]+h*((a2-(c2*v_euler[i])/(u_euler[i]+k2))*v_euler[i]))
+    w_euler.append(w_euler[i] + h * lmda*(p*m*q*u_euler[i]-c)*w_euler[i])
+    u_prim_euler.append(( (a1 - b1 * u_euler[i] - ((c1*v_euler[i]) / (u_euler[i]+k1))) * u_euler[i] - m*q * u_euler[i] * w_euler[i]))
+    v_prim_euler.append((a2-(c2*v_euler[i])/(u_euler[i]+k2))*v_euler[i])
+    w_prim_euler.append( lmda*(p*m*q*u_euler[i]-c)*w_euler[i] )
+
+
+########################################### Visualisation ##########################################
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.plot3D(solU, solV, solW, 'red')
@@ -65,21 +95,6 @@ plt.title("Résolution avec Scipy")
 plt.legend()
 plt.show()
 
-
-u_euler = [u0]
-v_euler = [v0]
-w_euler = [w0]
-u_prim_euler = [0]
-v_prim_euler = [0]
-w_prim_euler = [0]
-h = t_final/N
-for i in range(len(LT)-1):
-    u_euler.append(u_euler[i]  + h * ( (a1 - b1 * u_euler[i] - ((c1*v_euler[i]) / (u_euler[i]+k1))) * u_euler[i] - m*q * u_euler[i] * w_euler[i]) )
-    v_euler.append(v_euler[i]+h*((a2-(c2*v_euler[i])/(u_euler[i]+k2))*v_euler[i]))
-    w_euler.append(w_euler[i] + h * lmda*(p*m*q*u_euler[i]-c)*w_euler[i])
-    u_prim_euler.append(( (a1 - b1 * u_euler[i] - ((c1*v_euler[i]) / (u_euler[i]+k1))) * u_euler[i] - m*q * u_euler[i] * w_euler[i]))
-    v_prim_euler.append((a2-(c2*v_euler[i])/(u_euler[i]+k2))*v_euler[i])
-    w_prim_euler.append( lmda*(p*m*q*u_euler[i]-c)*w_euler[i] )
 
 
 plt.figure()
